c2e85f4450a7d6ffc0ed550d5272b5d54933b392
Patel Vivek <98373180+VivekPatel1706@users.noreply.github.com>
2024-11-09 14:35:00 +0000

    Added Home

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+url = st.secrets["database"]["DB_URL"]
+db = st.secrets["database"]["DB_NAME"]
+admin_collection = st.secrets["database"]["DB_ADMIN"]
+user_collection = st.secrets["database"]["DB_USER"]
+
+st.write("# Welcome to NutriScanAI! ")
+st.markdown("NutriScanAI is virtually an AI-Powered Web-Based application that scans a product's ingredient label to identify banned harmful substances in other countries.")
+st.markdown("It is AI usage to scan any given ingredients for possible health risks, identify banned substances across different countries.")
+st.markdown("NutriScanAI simplifies food safety information in real-time with honest data to help users make healthier choices.")
